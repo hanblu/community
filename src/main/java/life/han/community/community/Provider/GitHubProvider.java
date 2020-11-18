bc4dc2c087233b68b8729520c2001335994e781b
hanblu <han1976990247@163.com>
2020-11-18 09:49:46 +0800

    add readme

--- src/main/java/life/han/community/community/Provider/GitHubProvider.java
+++ src/main/java/life/han/community/community/Provider/GitHubProvider.java
@@ -1,4 +1,49 @@
 package life.han.community.community.Provider;
 
+
+
+import com.alibaba.fastjson.JSON;
+import life.han.community.community.dto.AccesstokenDTO;
+import life.han.community.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
 public class GitHubProvider {
-}
+    public String getAccessToken(AccesstokenDTO accesstokenDTO){
+       OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+            RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accesstokenDTO));
+            Request request = new Request.Builder()
+                    .url("https://github.com/login/oauth/access_token")
+                    .post(body)
+                    .build();
+            try (Response response = client.newCall(request).execute()) {
+                String string = response.body().string();
+                System.out.println(string);
+                return string;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+    }
+    public GithubUser githubUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return  null;
+        }
+    }
+    }
+
--- src/main/java/life/han/community/community/controller/AuthorizeController.java
+++ src/main/java/life/han/community/community/controller/AuthorizeController.java
@@ -1,14 +1,27 @@
 package life.han.community.community.controller;
 
+import life.han.community.community.Provider.GitHubProvider;
+import life.han.community.community.dto.AccesstokenDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizeController {
+    @Autowired
+    private GitHubProvider gitHubProvider;
     @GetMapping("/callback")
+
     public  String callback(@RequestParam(name="code") String code,
                             @RequestParam(name="state") String state){
+        AccesstokenDTO accesstokenDTO = new AccesstokenDTO();
+        accesstokenDTO.setCode(code);
+        accesstokenDTO.setRedirect_uri("http://localhost:8888/callback");
+        accesstokenDTO.setState(state);
+        accesstokenDTO.setClient_id("931c7ff5a6cef5c89d08");
+        accesstokenDTO.setClient_secret(" f3a90ffaca84cb85e75db963e1bf0960b09d7f82");
+        gitHubProvider.getAccessToken(accesstokenDTO);
         return "index";
     }
 }
